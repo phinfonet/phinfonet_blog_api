efca13c1f7a6ddd42fe535f705d442a1e3e3eeac
Pedro Henrique <pedrohenriqueacruz@gmail.com>
2015-08-26 21:57:07 -0300

    changed pages function and put app to serve static json

--- src/phinfonet_blog_api.c
+++ src/phinfonet_blog_api.c
@@ -1,11 +1,18 @@
 #include <kore/kore.h>
 #include <kore/http.h>
+#include "assets.h"
 
-int		page(struct http_request *);
+int	blog_posts(struct http_request *);
 
-int
-page(struct http_request *req)
-{
-	http_response(req, 200, NULL, 0);
+int blog_posts(struct http_request *req) {
+  if (req->method != HTTP_METHOD_GET) {
+    http_response_header(req, "allow", "GET");
+		http_response(req, 405, NULL, 0);
+    return (KORE_RESULT_OK);
+  }
+  
+	http_response_header(req, "content-type", "application/json");
+	http_response(req, 200, asset_home_json, asset_len_home_json);
+	kore_log(LOG_NOTICE, "GET BLOG POSTS");
 	return (KORE_RESULT_OK);
 }
